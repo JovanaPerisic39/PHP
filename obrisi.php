5af42f1cf296075aeebb5334f9a6a080c05d25df
Jovana Perisic <jovanaperisic39@gmail.com>
2022-10-15 14:07:52 +0200

    Second commit

---
+++ obrisi.php
@@ -0,0 +1,96 @@
+!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Sweet creations</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <link href="img/favicon.ico" rel="icon">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Teko:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border position-relative text-primary" style="width: 6rem; height: 6rem;" role="status"></div>
+        <img class="position-absolute top-50 start-50 translate-middle" src="img/icons/icon-1.png" alt="Icon">
+    </div>
+
+
+
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+        <a href="index.php" class="navbar-brand ms-4 ms-lg-0">
+            <h1 class="text-primary m-0"> </h1>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.php" class="nav-item nav-link">Pocetna</a>
+                <a href="dodaj.php" class="nav-item nav-link">Dodaj u ponudu</a>
+                <a href="izmeni.php" class="nav-item nav-link">Izmeni cenu</a>
+                <a href="obrisi.php" class="nav-item nav-link">Obrisi iz ponude</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <h1 class="display-5 mb-4">Momenti koji se pamte</h1>
+                <h3 class="text-primary"><?= $poruka; ?></h3>
+            </div>
+            <div class="row">
+                <form method="post" action="">
+                    <label for="poslastica">Poslastica</label>
+                    <select id="poslastica" name="poslastica" class="form-control">
+                        <?php
+                        foreach ($poslastice as $poslastica){
+                            ?>
+                        <option value="<?= $poslastica->id ?>"><?= $poslastica->naziv . " " ?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                    <hr/>
+                    <button type="submit" style="border-radius: 40px; border: 1px solid grey; background-color: rgb(221, 203, 217)" name="obrisi">Obrisi</button>
+
+                </form>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+
+        </div>
+    </div>
+
+    <div class="footerLogo"></div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="js/main.js"></script>
+
+
+    <script>
+        function popuniKomboTipova() {
+
+            $.ajax({
+                url: 'tipovi.php',
+                success: function (data) {
+                   $("#tip").html(data);
+                }
+            });
+        }
+        popuniKomboTipova();
+    </script>
+
+</body>
+
+</html>
